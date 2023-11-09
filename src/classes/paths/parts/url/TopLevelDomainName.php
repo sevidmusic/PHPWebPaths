78fcfb7340ec26dea0d303ebd0263e225d9df167
sevidmusic <sevidmusic@gmail.com>
2023-11-09 09:29:26 -0500

    Code clean up / documentation

--- src/classes/paths/parts/url/TopLevelDomainName.php
+++ src/classes/paths/parts/url/TopLevelDomainName.php
@@ -12,7 +12,9 @@ final class TopLevelDomainName implements TopLevelDomainNameInterface
     public function __construct(private Name $name) {
         $filteredName = new NameInstance(
             new Text(
-                strtolower(str_replace('_', '-', $name->__toString()))
+                strtolower(
+                    str_replace('_', '-', $name->__toString())
+                )
             )
         );
         $this->name = $filteredName;
